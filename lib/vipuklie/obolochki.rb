7db34ebaea6de2bcd805a708754a904e8bbbd02e
IQueue <aleva@sfedu.ru>
2023-06-04 22:56:35 +0300

    added test file and function graham_scan & jarvis_march

--- lib/vipuklie/obolochki.rb
+++ lib/vipuklie/obolochki.rb
@@ -3,5 +3,70 @@ require_relative "obolochki/version"
 module Vipuklie
   module Obolochki
     # Your code goes here...
+    class Error < StandardError; end
+    class Algorithms
+
+      def graham_scan(points)
+        # Находим нижнюю левую точку в массиве точек
+        start = points.min_by { |p| [p[1], p[0]] }
+      
+        # Сортируем остальные точки по полярному углу относительно начальной точки
+        sorted_points = points.sort_by { |p| [Math.atan2(p[1] - start[1], p[0] - start[0]), p[0], p[1]] }
+      
+        stack=[]
+        if points.size>=3
+        # Инициализируем стек с первыми двумя точками
+          stack = [start, sorted_points[1]]
+        
+      
+          # Проходим по остальным точкам и проверяем их вхождение в выпуклую оболочку
+          sorted_points[2..-1].each do |p|
+            while stack.size > 1 && ccw(stack[-2], stack[-1], p) <= 0
+              stack.pop
+            end
+            stack.push(p)
+          end
+        end
+
+
+        stack
+      end
+      
+      # Вспомогательная функция для определения ориентации точек
+      def ccw(p1, p2, p3)
+        (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
+      end
+      
+     
+
+      def jarvis_march(points)
+        # Находим самую левую точку в массиве точек
+        start = points.min_by { |p| p[0] }
+      
+        # Инициализируем массив, в котором будем хранить выпуклую оболочку
+        hull = []
+      
+        if (points.size>=3)
+        current = start
+        loop do
+          hull << current
+          next_point = points[0]
+          points.each do |p|
+            if p != current && (next_point == current || ccw(current, next_point, p) < 0)
+              next_point = p
+            end
+          end
+          current = next_point
+          break if current == start
+        end
+        end
+      
+        hull
+      end
+      
+      
+   
+    end
+        
   end
 end
--- test/vipuklie/obolochki_test.rb
+++ test/vipuklie/obolochki_test.rb
@@ -1,11 +1,89 @@
-require "test_helper"
+#require "test_helper"
+require "test/unit"
+require_relative '..\..\lib\vipuklie\obolochki.rb'
 
-class Vipuklie::ObolochkiTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Vipuklie::Obolochki::VERSION
+class TestHulls < Test::Unit::TestCase
+  def setup
+    @algorithms = Vipuklie::Obolochki::Algorithms.new
   end
 
-  def test_it_does_something_useful
-    assert false
+  test "test_jarvis_algorithm" do
+    points = [
+      [3, 0],
+      [4, 2],
+      [0, 7],
+      [4,4],
+      [2, 3],
+      [5, 6],
+      [2, 8]
+    ]
+
+    expected_hull = [
+      [0, 7], [3, 0], [4, 2], [5, 6], [2, 8]
+    ]
+
+    assert_equal expected_hull, @algorithms.jarvis_march(points)
+  end
+
+  test "test_jarvis_algorithm_empty" do
+    points = [
+      [0, 0],
+      [1, 1],
+    ]
+
+    expected_hull = []
+
+    assert_equal expected_hull, @algorithms.jarvis_march(points)
+  end
+
+  test "test_graham_algorithm" do
+    points = [
+      [-7, 8],
+      [-4, 6],
+      [2, 6],
+      [6, 4],
+      [8, 6],
+      [7, -2],
+      [4, -6],
+      [8, -7],
+      [0, 0],
+      [3, -2],
+      [6, -10],
+      [0, -6],
+      [-9, -5],
+      [-8, -2],
+      [-8, 0],
+      [-10, 3],
+      [-2, 2],
+      [-10, 4]
+    ]
+
+    expected_hull = [
+      [6, -10], [8, -7], [8, 6], [-7, 8], [-10, 4], [-10, 3], [-9, -5]
+    ]
+
+    assert_equal expected_hull, @algorithms.graham_scan(points)
+  end
+
+  test "test_graham_algorithm_empty" do
+    points = [[0, 0], [1, 1]]
+    assert_empty(@algorithms.graham_scan(points))
+  end
+
+  test "test_graham_algorithm_another" do
+    points = [
+      [0, 3],
+      [1, 1],
+      [2, 2],
+      [4, 4],
+      [0, 0],
+      [1, 2],
+      [3, 1],
+      [3, 3],
+    ]
+
+    expected_hull = [[0, 0], [3, 1], [4, 4], [0, 3]]
+
+    assert_equal expected_hull, @algorithms.graham_scan(points)
   end
 end
